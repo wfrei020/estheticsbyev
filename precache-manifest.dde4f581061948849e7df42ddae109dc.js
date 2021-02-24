f1ec3fac3af89e962bdd2617614a6a49e7f30d04
Andrei Freire <andrei@Andreis-MacBook-Pro.local>
2021-02-24 17:34:28 -0500

    Updates

---
+++ precache-manifest.dde4f581061948849e7df42ddae109dc.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0acd5c0ba059ef028d2365eae2dbbe89",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "36e6760cfe6943f671b8",
+    "url": "/react-gh-pages/static/css/main.b0ff4f73.chunk.css"
+  },
+  {
+    "revision": "8e55adcd80f1ffb165cc",
+    "url": "/react-gh-pages/static/js/2.1f95af94.chunk.js"
+  },
+  {
+    "revision": "8dbc9bd92b344a5deca62eeacc09fc2d",
+    "url": "/react-gh-pages/static/js/2.1f95af94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36e6760cfe6943f671b8",
+    "url": "/react-gh-pages/static/js/main.510e64c8.chunk.js"
+  },
+  {
+    "revision": "53d8e1d9d5ea61bcc9ac",
+    "url": "/react-gh-pages/static/js/runtime-main.3dbf2ff3.js"
+  },
+  {
+    "revision": "c86dd792af544e0b04b3b481670f7a9a",
+    "url": "/react-gh-pages/static/media/Relax-Renew-Spa-Panel-2-Background.c86dd792.jpg"
+  },
+  {
+    "revision": "5eeaab6850aab5765ef94b0b34904267",
+    "url": "/react-gh-pages/static/media/Tomatoes-O8L8.5eeaab68.ttf"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/react-gh-pages/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/react-gh-pages/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/react-gh-pages/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/react-gh-pages/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/react-gh-pages/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/react-gh-pages/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/react-gh-pages/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/react-gh-pages/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/react-gh-pages/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/react-gh-pages/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/react-gh-pages/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "08e18cb904f0f3c6dd9e3d4ed72e0eca",
+    "url": "/react-gh-pages/static/media/landing.08e18cb9.jpg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/react-gh-pages/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/react-gh-pages/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/react-gh-pages/static/media/profile-bg.baf6b40a.jpg"
+  },
+  {
+    "revision": "090a5aabae505f67ee0981613d02ee05",
+    "url": "/react-gh-pages/static/media/profile.090a5aab.jpg"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/react-gh-pages/static/media/sign.cf8b686b.jpg"
+  },
+  {
+    "revision": "ae0150c08dbcc95e4f50458e02e5bd5c",
+    "url": "/react-gh-pages/static/media/studio-1.ae0150c0.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/react-gh-pages/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/react-gh-pages/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/react-gh-pages/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/react-gh-pages/static/media/studio-5.ef5c30ea.jpg"
+  }
+]);
